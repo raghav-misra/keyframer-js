1a7873fbda59bc3e8f1e9a623a8845380db678b7
mrcool4000 <raghav.m2014@gmail.com>
2019-09-14 23:48:34 -0700

    Docs UI

---
+++ docs/assets/nav.js
@@ -0,0 +1,37 @@
+menuOpen = false;
+
+function openMenu(){
+    if(menuOpen) return;
+    menuOpen = true;
+    document.querySelector("#closeNav").disabled = false;
+    document.querySelector("#title").style.display = "none";
+    document.querySelector("#title").disabled = true;
+    keyframer.createCSSTransition("header", "all 0.5s ease-in-out", {
+        background: "#242a30",
+        borderRadius: "0"
+    }, ()=>{
+        keyframer.createCSSTransition("#nav", " height 1s ease-in-out", {
+            height: "300px"
+        });
+    });  
+};
+
+function closeMenu(){
+    menuOpen = false;
+    document.querySelector("#closeNav").disabled = true;
+    keyframer.createCSSTransition("header", "all 0.5s ease-in-out", {
+        background: "white",
+        borderRadius: "50px"
+    }, ()=>{
+        keyframer.createCSSTransition("#nav", " height 1s ease-in-out", {
+            height: "0px",
+        });
+    });
+    setTimeout(()=>{
+        document.querySelector("#title").style.display = "block";
+        document.querySelector("#title").disabled = true;
+    }, 1000);
+}
+
+document.querySelector("#title").onclick = openMenu;
+document.querySelector("#closeNav").onclick = closeMenu;
